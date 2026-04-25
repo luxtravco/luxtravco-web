c4d894fba611185f96775e94da6d9114bff7f5e2
luxtravco <projectaero.feedback@gmail.com>
2026-04-25 13:19:16 -0700

    Delete supabase-client.js

--- supabase-client.js
+++
@@ -1,8 +0,0 @@
-window.luxSupabaseReady = import(
-  'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
-).then(({ createClient }) => {
-  const SUPABASE_URL = 'https://vmphayezatepxjauxhcd.supabase.co';
-  const SUPABASE_KEY = 'sb_publishable_TwCvQ_u0VglyXCy6Sgciwg_3XulTaU1';
-  window.luxSupabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-  return window.luxSupabase;
-});
